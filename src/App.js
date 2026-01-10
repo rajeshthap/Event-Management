45bbb83125d79d6f96269a8934a6a46c71cbd5ef
Deepika <deepikachauhan8988@gmail.com>
2026-01-10 15:43:32 +0530

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fontsource/poppins";
+import "bootstrap-icons/font/bootstrap-icons.css";
+// import "../src/custom/style.css";
+import {
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+// import { AuthProvider } from "./context/AuthContext";
+
+
+import Home from "./componets/pages/Home";
+import Footer from "./componets/footer/Footer";
+import NavBar from "./componets/navbar/NavBar";
+// import NavBar from "./componets/topnav/NavBar";
+// import Footer from "./componets/footer/Footer";
+// import Dashboard from "./componets/dash_board/Dashboard";
 
 function App() {
+  const location = useLocation();
+
+  const hiddenPaths = new Set([""]);
+
+  const shouldHideNavbar = hiddenPaths.has(location.pathname);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="app-container">
+        {!shouldHideNavbar && <NavBar />}
+        
+        <main className="main-content">
+          <Routes>
+            {/* Public Route */}
+            <Route path="/" element={<Home />} />
+            
+          
+          </Routes>
+        </main>
+        
+        <Footer />
+      </div>
+ 
   );
 }
 
