e7b67fd23e64aff63fdbba7a0b5600336bda3a4d
Deepika <deepikachauhan8988@gmail.com>
2026-01-10 17:04:20 +0530

    Home and About section done

--- src/componets/pages/Home.js
+++ src/componets/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Carousel from './EventCarousel'
+import AboutUs from './AboutUs'
 
 function Home() {
   return (
     <div className='main'>
 <Carousel />
+<AboutUs />
     </div>
   )
 }
